884fdf9139499958fd051539cee1eb9a40797ec7
PhantomKing99999 <LiuQinAnAndy@gmail.com>
2024-11-07 20:09:46 +0800

    Revert "Test"
    This reverts commit a5e287757dd5d089eb1fb453d9d643b8fe4e02b1.

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,9 @@
-#include <iostream>
+#include <bits/stdc++.h>
 #include <vex.h>
 #include <autonomous.hpp>
 #include <bot.hpp>
 #include <config.hpp>
 #include <pathing.hpp>
-
-controller controller1=controller(primary);
-
-//intake
-motor MotorI1 = motor(PORT1, ratio6_1, true); //ports and names temp
-motor MotorI2 = motor(PORT1, ratio6_1, false); //ports and names temp
-
-pneumatic Pnuematic = pneumatic(PORT1, false); //port temp
-bool pumpstate=false;
-
-optical Optical = optical(PORT1); //port temp
-color detectColor=Optical.color();
 
 void autonomous_task() {
     base.follow_path({0, 100, 50, 0}, {0, 50, 100, 50}, 0.5);
@@ -36,36 +24,7 @@ void control_task() {
         turn_speed = controller.Axis1.position();
         left.spin(vex::fwd, linear_speed + turn_speed, vex::pct);
         right.spin(vex::fwd, linear_speed - turn_speed, vex::pct);
-
-        Controller.ButtonA.pressed(buttonPressed);
-        MotorI1.spin(forward);
-        MotorI2.spin(reverse); //if it's set to reverse in the definition does it need to be reverse here too?? or is it like double negatives
-        if (Optical.isNearObject())
-        {
-            detectColor=Optical.color();
-            if(detectColor==blue) //temp color
-            {
-                MotorI1.stop();
-                MotorI2.stop();
-                vexDelay(5);
-            }
-        }
-        
         vex::this_thread::sleep_for(10);
-    }
-}
-
-void buttonPressed()
-{
-    if(pumpstate)
-    {
-        Pneumatic.pumpOff();
-        pumpstate=false;
-    }
-    else 
-    {
-        Pneumatic.pumpOn();
-        pumpstate=true;
     }
 }
 
