2adbc76f92cafbe6adc37b0f6e884590cc5b6336
ericssonl07 <ljh07ericsson@gmail.com>
2024-11-07 15:11:34 +0800

    Fixed some gear ratio issues, etc.

--- include/config.hpp
+++ include/config.hpp
@@ -4,7 +4,7 @@
     * 2- competition (6 motors)
     * Any other mode will result in compilation error
 */
-#define MODE 0
+#define MODE 1
 
 #define PAUSE(x) vexDelay(x);
 #define SEC * 1000
@@ -16,6 +16,11 @@
 #define RAD
 #define DEG * 0.0174532925
 
+// Modes and keybinds
+#define brakeMode vex::brakeType::brake
+#define intakeKeybind controller.ButtonA
+#define outtakeKeybind controller.ButtonX
+#define pneumaticKeybind controller.ButtonY
 
 vex::brain brain;
 vex::controller controller;
@@ -24,14 +29,19 @@ vex::motor lm1(vex::PORT1, vex::gearSetting::ratio36_1, false);
 vex::motor rm1(vex::PORT2, vex::gearSetting::ratio36_1, true);
 AbstractMotorGroup right(&rm1), left(&lm1);
 #elif MODE >= 1 and MODE <= 2 // Full configuration
-vex::motor rm1(vex::PORT1, vex::ratio36_1, false), rm2(vex::PORT2, vex::ratio36_1, false), rm3(vex::PORT3, vex::ratio36_1, false);
-vex::motor lm1(vex::PORT4, vex::ratio36_1, true), lm2(vex::PORT5, vex::ratio36_1, true), lm3(vex::PORT6, vex::ratio36_1, true);
+vex::motor rm1(vex::PORT13, vex::ratio6_1, false), rm2(vex::PORT12, vex::ratio6_1, false), rm3(vex::PORT11, vex::ratio6_1, false);
+vex::motor lm1(vex::PORT3, vex::ratio6_1, true), lm2(vex::PORT2, vex::ratio6_1, true), lm3(vex::PORT1, vex::ratio6_1, true);
+vex::motor intake_hook(vex::PORT14, vex::ratio18_1, false), intake_roller(vex::PORT20, vex::ratio18_1, true);
+vex::pneumatics mobile_goal(brain.ThreeWirePort.A);
 AbstractMotorGroup right(&rm1, &rm2, &rm3), left(&lm1, &lm2, &lm3);
 #endif
 BotBase base (
     &left, &right, /* Motor groups */ 
-    32.2 /* Base width */,              5.1 /* Wheel radius */,
-    12.5 /* Pursuit distance */,        0.1186440678 /* Gear multiplier */,
+    31.4 /* Base width */,              4.25 /* Wheel radius */,
+    12.5 /* Pursuit distance */,        0.052085391069929 /* Gear multiplier */, /*113/99*0.04563233376923-*/
     0.0, 0.0, 0.0 /* Initial pose */,   1 /* Thread sleep: 1 milliseconds */
 );
 vex::thread t(coordinate_display, (void *) &base);
+
+void autonomous_task();
+void control_task();
--- src/main.cpp
+++ src/main.cpp
@@ -5,29 +5,6 @@
 #include <config.hpp>
 #include <pathing.hpp>
 
-void autonomous_task() {
-    base.follow_path({0, 100, 50, 0}, {0, 50, 100, 50}, 0.5);
-    vexDelay(200 MSEC);
-    base.turn_to(90 DEG, 2.5 DEG);
-    vexDelay(200 MSEC);
-    base.forward(100);
-    base.turn(90 DEG, 2.5 DEG);
-}
-
-void control_task() {
-    std::map<std::string, std::function<void()>> keybinds = {
-        {} // e.g. {"A+B", [] () {base.forward(100);}}
-    };
-    double linear_speed = 0, turn_speed = 0;
-    while (true) {
-        linear_speed = controller.Axis3.position();
-        turn_speed = controller.Axis1.position();
-        left.spin(vex::fwd, linear_speed + turn_speed, vex::pct);
-        right.spin(vex::fwd, linear_speed - turn_speed, vex::pct);
-        vex::this_thread::sleep_for(10);
-    }
-}
-
 int main() {
     vexDelay(1); // DO NOT REMOVE!!! IMPORTANT FOR ODOMETRY TO ZERO
 
@@ -41,4 +18,60 @@ int main() {
     #endif
 
     while(true) vexDelay(1000); // DO NOT REMOVE!!! IMPORTANT TO KEEP THREADS ALIVE
+}
+
+void autonomous_task() {
+    //base.follow_path({0, 60, 30, 0}, {0, 30, 60, 30}, 0.5, 0.15);
+    /*
+    (0, 0)
+    (100, 0)
+    (100, 100)
+    */
+    base.follow_path({0, 50, 50}, {0, 0, 50}, 0.5, 0.35);
+    //vexDelay(200 MSEC);
+    //base.turn_to(90 DEG, 2.5 DEG);
+    //vexDelay(2 SEC);
+    //base.forward(35, 1.0, 0.1);
+    //base.turn_to(450 DEG, 2.5 DEG, 0.25);
+}
+
+void control_task() {
+    double linear_speed, turn_speed, left_speed, right_speed;
+    bool last_pneumatic = false, pneumatic_engaged = false;
+    while (true) {
+        linear_speed = controller.Axis3.position();
+        turn_speed = controller.Axis1.position();
+        left_speed = linear_speed + turn_speed;
+        right_speed = linear_speed - turn_speed;
+        if (fabs(left_speed) > 0.5) {
+            left.spin(vex::fwd, left_speed, vex::pct);
+        } else {
+            left.stop(brakeMode);
+        }
+        if (fabs(right_speed) > 0.5) {
+            right.spin(vex::fwd, right_speed, vex::pct);
+        } else {
+            right.stop(brakeMode);
+        }
+        if (intakeKeybind.pressing()) {
+            intake_hook.spin(vex::fwd, 100, vex::pct);
+            intake_roller.spin(vex::fwd, 100, vex::pct);
+        } else if (outtakeKeybind.pressing()) {
+            intake_hook.spin(vex::reverse, 100, vex::pct);
+            intake_roller.spin(vex::reverse, 100, vex::pct);
+        } else {
+            intake_hook.stop(brakeMode);
+            intake_roller.stop(brakeMode);
+        }
+        if (pneumaticKeybind.pressing() != last_pneumatic and last_pneumatic == false) {
+            if (pneumatic_engaged) {
+                mobile_goal.close();
+            } else {
+                mobile_goal.open();
+            }
+            pneumatic_engaged = not pneumatic_engaged;
+        }
+        last_pneumatic = pneumaticKeybind.pressing();
+        vex::this_thread::sleep_for(10);
+    }
 }
